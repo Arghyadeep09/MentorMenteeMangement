6108c1f8e904e06a08bde85b3bc3f4d455f698a1
Arghya <arghyahari2001@gmail.com>
2025-02-03 23:32:18 +0530

    server.js changed

--- Backend/server.js
+++ Backend/server.js
@@ -12,18 +12,18 @@ const menteeRoutes = require("./routes/menteeRoutes");
 const app = express();
 
 const corsOptions = {
-    origin: ["https://mentormenteemangement-1.onrender.com/", "http://localhost:5173"], // Allow these origins
+    origin: ["https://mentormenteemangement.onrender.com", "https://mentormenteemangement-1.onrender.com"], // Allow these origins
     credentials: true, // Allow cookies to be sent with requests
     allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
   };
   
   // Apply CORS to all routes
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.json());
   app.use(bodyParser.json());
 
 // Routes
-app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/auth", authRoutes);
 app.use('/api/mentor', mentorRoutes);
 app.use('/api/mentee',menteeRoutes)
 app.use("/api/users", userRoutes);
